8bb976eaa36f676aedeefc526161df8a434c533c
xrbww <49225863+style55smile@users.noreply.github.com>
2020-09-23 17:43:03 +0800

    Update 简单插入排序__升序.cpp

--- 插入排序/简单插入排序__升序.cpp
+++ 插入排序/简单插入排序__升序.cpp
@@ -1,16 +1,21 @@
-
 #include <iostream>
 using namespace std;
 void m_swap(int *a, int *b);
 void charu(int *a,int len) {
+	//定义的一个基准点
 	int pivot = 0;
+	//每次比较的数变多，第一个形成容量为1的有序数组，依次容量变成2、3、4、5、.....、len
 	for (int i = 0; i < len; i++)
 	{
+		//pivot是每次加入有序数组的数字下标。
 		pivot = i;
-		for (int j = i; j >= 0; j--)
+		//从有序数组的最后一位数字，向数组头进行比较
+		for (int j = i-1; j >= 0; j--)
 		{
+			//如果新加入的数字小于，数组中的数字，交换新数字与数组中数字的位置
 			if (a[pivot] < a[j]) {
 				m_swap(&a[pivot], &a[j]);
+				//更新基准点的坐标
 				pivot = j;
 			}
 		}
